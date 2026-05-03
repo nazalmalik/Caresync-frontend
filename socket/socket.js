77e5ded36ab9816d0f2222af044bace898b01f21
Nazal Malik <mnazal250@gmail.com>
2026-05-03 15:06:16 +0500

    backend url in socket.io page in frontend

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://caresync-backend-production-5065.up.railway.app/");
 
 export default socket;
